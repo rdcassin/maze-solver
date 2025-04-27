231bb81b99a04e3600565b64a7fc9b7f52d04d02
rdcassin <rdcassin@gmail.com>
2025-04-27 14:05:03 -0500

    Added maze reset test.

--- tests.py
+++ tests.py
@@ -15,5 +15,14 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
+    def test_maze_reset(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        m1._reset_cells_visited()
+        for i in range(num_cols):
+            for j in range(num_rows):
+                self.assertEqual(m1._cells[i][j].visited, False)
+
 if __name__ == "__main__":
     unittest.main()
